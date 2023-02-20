8aa72876ec19c8786cda41e41088301a10c2b50f
Max Sidorenko <fivemoshr@gmail.com>
2023-02-20 13:10:32 +0200

    add basename

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-// basename = 'react-homework-template';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="react-homework-template">
+    <BrowserRouter basename="ts-practice-react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
